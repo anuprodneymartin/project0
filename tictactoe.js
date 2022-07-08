ebe2dbd95363db5da73259e6c8db4bff95c55ee3
anup martin <anuprodney.martin@gmail.com>
2022-07-08 18:13:13 +1000

    project0

--- tictactoe.js
+++ tictactoe.js
@@ -4,24 +4,39 @@ $(document).ready(function (){
             $(this).text('x');
             $(this).css('color', '#00FF00');
             turns++
+            if(checkWinner()){
+                $('.game-over').text('Player 1  wins!')
+            }
         }else if ($(this).text() === '' && turns%2 !== 1) {
             $(this).text('0');
             $(this).css('color', '#FFFFF0');
             turns++
+            if(checkWinner()){
+                $('.game-over').text('Player 2 wins!')
+            }
         }
     });
-    $('#1').ready
-});
-
-let turns = 1;
+    let turns = 1;
 const winningCombinationDivs = [[1,2,3], [4, 5, 6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 
 const checkWinner = function(){
     for(let i=0; i<winningCombinationDivs.length; i++){
-
+        const winningCombination=winningCombinationDivs[i];
+        const id1 = winningCombination[0];
+        const id2 = winningCombination[1];
+        const id3 = winningCombination[2];
+        if(($('#' + id1).text() === 'x' && $('#' + id2).text() === 'x' && $('#' + id3).text() ==='x') || 
+        $('#' + id1).text() === "0" && $('#' + id2).text() === '0' && $('#' + id3).text() === '0') {
+            return true;
+        }
     }
+    return false;
 
 };
+    $('#1').ready
+});
+
+
 
 console.log('Hello')
 
