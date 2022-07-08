741f87184cea99744c44a189f42b1151abd74dda
anup martin <anuprodney.martin@gmail.com>
2022-07-08 16:59:30 +1000

    updated project0

---
+++ README.md
@@ -0,0 +1,6 @@
+# A game of 'Tic Tac Toe'.
+
+## I have tried my best to build my first game in the internet world with help from a friend named Loden. I know its not perfect but I am still working on the same.
+
+Play here: [weblink](https://anuprodneymartin.github.io/project0/)
+
--- tictactoe.js
+++ tictactoe.js
@@ -10,13 +10,18 @@ $(document).ready(function (){
             turns++
         }
     });
-
+    $('#1').ready
 });
 
 let turns = 1;
+const winningCombinationDivs = [[1,2,3], [4, 5, 6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 
-// if (theWinneris() !== -1 && || theWinneris () = !== '') {
-    
-// }
+const checkWinner = function(){
+    for(let i=0; i<winningCombinationDivs.length; i++){
+
+    }
+
+};
 
 console.log('Hello')
+
